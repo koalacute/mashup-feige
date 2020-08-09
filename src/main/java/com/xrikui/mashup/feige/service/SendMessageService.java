2552ed7c818e0da94de77a2c8642b06f647db0c0
七曦 <koalacute@aliyun.com>
2020-08-09 15:03:43 +0800

    优化股票监控功能

--- src/main/java/com/xrikui/mashup/feige/service/SendMessageService.java
+++ src/main/java/com/xrikui/mashup/feige/service/SendMessageService.java
@@ -1,26 +1,8 @@
 package com.xrikui.mashup.feige.service;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SendMessageService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    /**
-     * 发送通知服务
-     *
-     * @param object
-     * @param url
-     */
-    public void send(Object object, String url) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity httpEntity = new HttpEntity(object, httpHeaders);
-        restTemplate.postForEntity(url, httpEntity, String.class);
-    }
 }
